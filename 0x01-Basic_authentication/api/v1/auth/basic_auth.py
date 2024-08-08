f4dd02c248d673c879c4c4e96b54f04523a6357a
Your Name <you@example.com>
2024-08-08 02:46:57 -0700

    Task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,22 @@ class BasicAuth(Auth):
     """
     BasicAuth class for handling basic authentication
     """
-    pass
+
+    def extract_base64_authorization_header(
+                                            self,
+                                            authorization_header: str
+                                            ) -> str:
+        """
+        Extracts the Base64 part of the Authorization
+        header for a Basic Authentication.
+        Returns:
+            The Base64 part of the Authorization header if
+            valid, otherwise None.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
