450e65955d10ac45b1167b3b5e2d8e98f2309de9
Your Name <you@example.com>
2024-08-08 04:23:31 -0700

    Task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -50,3 +50,22 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+                                 self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """
+        Extracts user credentials from the decoded Base64 string.
+        Returns:
+            A tuple of the user email and password if valid,
+            otherwise (None, None).
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
