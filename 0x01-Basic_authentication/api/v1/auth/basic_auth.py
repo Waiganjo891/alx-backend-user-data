0d91944c2d639c46bef100215261cd908284a5f2
Your Name <you@example.com>
2024-08-09 04:22:18 -0700

    Task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -96,3 +96,27 @@ class BasicAuth(Auth):
         if not user.is_valid_password(user_pwd):
             return None
         return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the User instance for a request.
+        Args:
+            request: The request object.
+        Returns:
+            User: The User instance if authentication is successful,
+            otherwise None.
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        if base64_header is None:
+            return None
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        if decoded_header is None:
+            return None
+        email, password = self.extract_user_credentials(decoded_header)
+        if email is None or password is None:
+            return None
+        user = self.user_object_from_credentials(email, password)
+        return user
