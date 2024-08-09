2753b6db89968b842ab5e7b9a59b63c27c2a569b
Your Name <you@example.com>
2024-08-09 04:07:43 -0700

    Task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,20 +3,19 @@
 BasicAuth module for the API authentication
 """
 import base64
+from typing import TypeVar
 from api.v1.auth.auth import Auth
 from models.user import User
-from typing import TypeVar, Optional
 
 
 class BasicAuth(Auth):
     """
     BasicAuth class for handling basic authentication
     """
-
     def extract_base64_authorization_header(
                                             self,
                                             authorization_header: str
-                                            ) -> Optional[str]:
+                                            ) -> str:
         """
         Extracts the Base64 part of the Authorization
         header for a Basic Authentication.
@@ -35,7 +34,7 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(
                                            self,
                                            base64_authorization_header: str
-                                           ) -> Optional[str]:
+                                           ) -> str:
         """
         Decodes a Base64 string and returns the decoded value
         as a UTF-8 string.
@@ -56,7 +55,7 @@ class BasicAuth(Auth):
     def extract_user_credentials(
                                  self,
                                  decoded_base64_authorization_header: str
-                                 ) -> (Optional[str], Optional[str]):
+                                 ) -> (str, str):
         """
         Extracts user credentials from the decoded Base64 string.
         Returns:
@@ -73,25 +72,27 @@ class BasicAuth(Auth):
         return email, password
 
     def user_object_from_credentials(
-                                     self,
-                                     user_email: str,
-                                     user_pwd: str
-                                     ) -> TypeVar('User'):
+                                    self,
+                                    user_email: str,
+                                    user_pwd: str
+                                    ) -> TypeVar('User'):
         """
-        Returns the User instance based on his email
-        and password.
+        Returns the User instance based on his email and password.
+        Args:
+            user_email (str): The user's email address.
+            user_pwd (str): The user's password.
         Returns:
-            The User instance if found and credentials
+            User: The User instance if credentials
             are valid, otherwise None.
         """
         if user_email is None or not isinstance(user_email, str):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        user_list = User.search({'email': user_email})
-        if not user_list:
+        users = User.search({'email': user_email})
+        if not users:
             return None
-        user = user_list[0]
+        user = users[0]
         if not user.is_valid_password(user_pwd):
             return None
         return user
